495b64146226b7c3266d06b57dbbf2ad77fe2515
Deven V. Patel <Devenvimalpatel@gmail.com>
2020-02-10 13:25:37 -0800

    Update files to implement abstract class

--- server/database.py
+++ server/database.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 # pprint library is used to make the output look more pretty
 from pprint import pprint
+from abc import *
 
 # connect to MongoDB, TODO: Update to mongo and securly store info
 client = MongoClient("mongodb://User:Pass@notifier-shard-00-00-wcy1w.azure.mongodb.net:27017,notifier-shard-00-01-wcy1w.azure.mongodb.net:27017,notifier-shard-00-02-wcy1w.azure.mongodb.net:27017/test?ssl=true&replicaSet=Notifier-shard-0&authSource=admin&retryWrites=true")
@@ -13,49 +14,58 @@ db=client.dbName
 # ------------------------------------------------------------
 #                   Table operations
 #------------------------------------------------------------
+class Database_intf(ABC):
+    # create a document for the user in the database with the following information
+    # returns True on success and None on failure
+    @abstractmethod
+    def create_user(username, validation, salt, master_key):
+        raise NotImplementedError
 
-# create a document for the user in the database with the following information
-# returns True on success and None on failure
-def create_user(username, validation, salt, master_key):
-    pass
+    # remove a user and its data from the document
+    # returns True on success and None on failure
+    @abstractmethod
+    def delete_user(username):
+        raise NotImplementedError
 
-# remove a user and its data from the document
-# returns True on success and None on failure
-def delete_user(username):
-    pass
+    # given a user, add a key value pair and use the system time as the timestamp
+    # returns True on success and None on failure
+    @abstractmethod
+    def add_key_value_pair(username, key, value):
+        raise NotImplementedError
 
-# given a user, add a key value pair and use the system time as the timestamp
-# returns True on success and None on failure
-def add_key_value_pair(username, key, value):
-    pass
+    # given a user and a key, update the value and timestamp for the given key
+    # returns True on success and None on failure
+    @abstractmethod
+    def modify_key_value_pair(username, key, value):
+        raise NotImplementedError
 
-# given a user and a key, update the value and timestamp for the given key
-# returns True on success and None on failure
-def modify_key_value_pair(username, key, value):
-    pass
+    # given a user and a key, delete the value by setting it to null and update the timestamp
+    # returns True on success and None on failure
+    @abstractmethod
+    def delete_key_value_pair(username, key):
+        raise NotImplementedError
 
-# given a user and a key, delete the value by setting it to null and update the timestamp
-# returns True on success and None on failure
-def delete_key_value_pair(username, key):
-    pass
+    # given a user and a key, get the value from the database
+    # returns the value on success and None on failure
+    @abstractmethod
+    def get_keys_given_user(username):
+        raise NotImplementedError
 
-# given a user and a key, get the value from the database
-# returns the value on success and None on failure
-def get_keys_given_user(username):
-    pass
+    # given a user, return a list of the keys associated with him
+    # returns a list of keys on success, and None on failure
+    @abstractmethod
+    def get_keys_given_user(username):
+        raise NotImplementedError
 
-# given a user, return a list of the keys associated with him
-# returns a list of keys on success, and None on failure
-def get_keys_given_user(username):
-    pass
-
-# given a user and a key, return the last time that key was modified
-# returns a timestamp with millisecond granularity since Epoch on success, and None on failure
-def get_modified_time(username, key):
-    pass
+    # given a user and a key, return the last time that key was modified
+    # returns a timestamp with millisecond granularity since Epoch on success, and None on failure
+    @abstractmethod
+    def get_modified_time(username, key):
+        raise NotImplementedError
 
 
-# given a user, return a list of the keys associated with him that have null values
-# returns a list of keys with null values on success, and None on failure
-def get_null_keys_given_user(username):
-    pass
+    # given a user, return a list of the keys associated with him that have null values
+    # returns a list of keys with null values on success, and None on failure
+    @abstractmethod
+    def get_null_keys_given_user(username):
+        raise NotImplementedError
--- server/database_test.py
+++ server/database_test.py
@@ -1,92 +1,95 @@
 # pprint library is used to make the output look more pretty
 from pprint import pprint
 import time
+from database import Database_intf
 
-test_dict = {}
 
-# ------------------------------------------------------------
-#                   Table operations
-#------------------------------------------------------------
 
-# create a document for the user in the database with the following information
-# returns True on success and None on failure
-def create_user(username, validation, salt, master_key):
-    user_count = len(test_dict.keys())
-    test_dict[user_count + 1] = {        
-        "username":username,
-        "hashed_validation":validation,
-        "salt":salt,
-        "encrypted_master_key":master_key,
-        "logins":{}
-    }
+class database_test(Database_intf):
+    self.test_dict = {}
+    # ------------------------------------------------------------
+    #                   Table operations
+    #------------------------------------------------------------
 
-# remove a user and its data from the document
-# returns True on success and None on failure
-def delete_user(username):
-    for id in test_dict.keys():
-        if test_dict[id]["username"] == username:
-            del test_dict[id]
-            return True
-    return None
+    # create a document for the user in the database with the following information
+    # returns True on success and None on failure
+    def create_user(username, validation, salt, master_key):
+        user_count = len(self.test_dict.keys())
+        self.test_dict[user_count + 1] = {        
+            "username":username,
+            "hashed_validation":validation,
+            "salt":salt,
+            "encrypted_master_key":master_key,
+            "logins":{}
+        }
 
-# given a user, add a key value pair and use the system time as the timestamp
-# returns True on success and None on failure
-def add_key_value_pair(username, key, value):
-    for id in test_dict.keys():
-        if test_dict[id]["username"] == username:
-            test_dict[id]["logins"][key] = (value, time.time() * 1000)
-            return True
-    return None
-
-# given a user and a key, update the value and timestamp for the given key
-# returns True on success and None on failure
-def modify_key_value_pair(username, key, value):
-    return add_key_value_pair(username, key, value)
-
-# given a user and a key, delete the value by setting it to null and update the timestamp
-# returns True on success and None on failure
-def delete_key_value_pair(username, key):
-    for id in test_dict.keys():
-        if test_dict[id]["username"] == username:
-            if key in test_dict[id]["logins"]:
-                test_dict[id]["logins"][key] = (None, time.time() * 1000)
+    # remove a user and its data from the document
+    # returns True on success and None on failure
+    def delete_user(username):
+        for id in self.test_dict.keys():
+            if self.test_dict[id]["username"] == username:
+                del self.test_dict[id]
                 return True
-    return None
+        return None
 
-# given a user and a key, get the value from the database
-# returns the value on success and None on failure
-def get_value_given_user_and_key(username, key):
-    for id in test_dict.keys():
-        if test_dict[id]["username"] == username:
-            if key in test_dict[id]["logins"]:
-                return test_dict[id]["logins"][key][0]
-    return None
+    # given a user, add a key value pair and use the system time as the timestamp
+    # returns True on success and None on failure
+    def add_key_value_pair(username, key, value):
+        for id in self.test_dict.keys():
+            if self.test_dict[id]["username"] == username:
+                self.test_dict[id]["logins"][key] = (value, time.time() * 1000)
+                return True
+        return None
 
-# given a user, return a list of the keys associated with him
-# returns a list of keys on success, and None on failure
-def get_keys_given_user(username):
-    for id in test_dict.keys():
-        if test_dict[id]["username"] == username:
-            return test_dict[id]["logins"].keys()
-    return None
+    # given a user and a key, update the value and timestamp for the given key
+    # returns True on success and None on failure
+    def modify_key_value_pair(username, key, value):
+        return add_key_value_pair(username, key, value)
 
-# given a user and a key, return the last time that key was modified
-# returns a timestamp with millisecond granularity since Epoch on success, and None on failure
-def get_modified_time(username, key):
-    for id in test_dict.keys():
-        if test_dict[id]["username"] == username:
-            if key in test_dict[id]["logins"]:
-                return test_dict[id]["logins"][key][1]
-    return None
+    # given a user and a key, delete the value by setting it to null and update the timestamp
+    # returns True on success and None on failure
+    def delete_key_value_pair(username, key):
+        for id in self.test_dict.keys():
+            if self.test_dict[id]["username"] == username:
+                if key in self.test_dict[id]["logins"]:
+                    self.test_dict[id]["logins"][key] = (None, time.time() * 1000)
+                    return True
+        return None
+
+    # given a user and a key, get the value from the database
+    # returns the value on success and None on failure
+    def get_value_given_user_and_key(username, key):
+        for id in self.test_dict.keys():
+            if self.test_dict[id]["username"] == username:
+                if key in self.test_dict[id]["logins"]:
+                    return self.test_dict[id]["logins"][key][0]
+        return None
+
+    # given a user, return a list of the keys associated with him
+    # returns a list of keys on success, and None on failure
+    def get_keys_given_user(username):
+        for id in self.test_dict.keys():
+            if self.test_dict[id]["username"] == username:
+                return self.test_dict[id]["logins"].keys()
+        return None
+
+    # given a user and a key, return the last time that key was modified
+    # returns a timestamp with millisecond granularity since Epoch on success, and None on failure
+    def get_modified_time(username, key):
+        for id in self.test_dict.keys():
+            if self.test_dict[id]["username"] == username:
+                if key in self.test_dict[id]["logins"]:
+                    return self.test_dict[id]["logins"][key][1]
+        return None
 
 
-# given a user, return a list of the keys associated with him that have null values
-# returns a list of keys with null values on success, and None on failure
-def get_null_keys_given_user(username):
-    for id in test_dict.keys():
-        if test_dict[id]["username"] == username:
-            return [key for key in test_dict[id]["logins"].keys() if test_dict[id]["logins"][key][0] is None]
-    return None
+    # given a user, return a list of the keys associated with him that have null values
+    # returns a list of keys with null values on success, and None on failure
+    def get_null_keys_given_user(username):
+        for id in self.test_dict.keys():
+            if self.test_dict[id]["username"] == username:
+                return [key for key in self.test_dict[id]["logins"].keys() if self.test_dict[id]["logins"][key][0] is None]
+        return None
 
-def print_dict():
-    pprint(test_dict)
+    def print_dict():
+        pprint(self.test_dict)
