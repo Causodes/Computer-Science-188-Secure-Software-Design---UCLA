7475959a82c6920f9e6929e0177ede2ce7e0e0ec
Causodes <tianyesh@gmail.com>
2020-02-18 16:32:10 -0800

    Finish pasword recovery security questions page

--- application/ui.py
+++ application/ui.py
@@ -5,13 +5,26 @@ import sys, os, platform
 
 
 TRUE_FONT = "Arial"
-assetdir = os.path.join(os.path.dirname(__file__), 'assets')
+_assetdir = os.path.join(os.path.dirname(__file__), 'assets')
+_security_questions_1 = ['Are you single? If so, why?',
+                         'Why did you forget your password?',
+                         'What is your favorite color?',
+                         'What is your mother\'s maiden name?',
+                         'Who is your favorite CS professor?']
+_security_questions_2 = ['What is your favorite TV program?',
+                         'What team do you love to see lose?',
+                         'Where did you meet your spouse?',
+                         'At what age did you lose your virginity?',
+                         'Where did you have your first kiss?']
 
 # Utility functions
 def _log_in():
     raise NotImplementedError
     
 def _clear_entry(username_entry, pw_entry, pw_confirm_entry):
+    raise NotImplementedError
+    
+def _reset_password(answer_1, answer_2, new_password):
     raise NotImplementedError
 
 def _combine_funcs(*funcs):
@@ -77,7 +90,7 @@ class StartPage(tk.Frame):
         self.config(bg='#FFFFFF')
         
         # import logo 
-        iconfile = os.path.join(assetdir, 'black_noodles_black.png')
+        iconfile = os.path.join(_assetdir, 'black_noodles_black.png')
         image = Image.open(iconfile)
         logo_resized = image.resize((200, 200), Image.ANTIALIAS)     
         img = ImageTk.PhotoImage(logo_resized)
@@ -85,7 +98,7 @@ class StartPage(tk.Frame):
         logo.image = img # prevent garbage collection
         
         # import entryline image
-        entryline_file = os.path.join(assetdir, 'entryline.png')
+        entryline_file = os.path.join(_assetdir, 'entryline.png')
         entryline_image = Image.open(entryline_file)
         entryline_resized = entryline_image.resize((250, 26), Image.ANTIALIAS)
         entryline_final = ImageTk.PhotoImage(entryline_resized)
@@ -106,7 +119,7 @@ class StartPage(tk.Frame):
         forgot_pw_button = HoverButton(self, text="Forgot Password?", padx=10, pady=10, command=lambda: controller.show_frame(ForgotPassword), background='#FFFFFF', foreground='#757575', activebackground='#FFFFFF', activeforeground='#40c4ff', borderwidth=0)
         
         # login button
-        log_in_button_path = os.path.join(assetdir, 'log_in.png')
+        log_in_button_path = os.path.join(_assetdir, 'log_in.png')
         log_in_button_image = Image.open(log_in_button_path)
         log_in_button_resized = log_in_button_image.resize((250, 47), Image.ANTIALIAS)
         log_in_button_final = ImageTk.PhotoImage(log_in_button_resized)
@@ -114,7 +127,7 @@ class StartPage(tk.Frame):
         log_in_button.image = log_in_button_final # prevent garbage collection
 
         # sign up button
-        sign_up_button_path = os.path.join(assetdir, 'sign_up_home_page.png')
+        sign_up_button_path = os.path.join(_assetdir, 'sign_up_home_page.png')
         sign_up_button_image = Image.open(sign_up_button_path)
         sign_up_button_resized = sign_up_button_image.resize((250, 47), Image.ANTIALIAS)
         sign_up_button_final = ImageTk.PhotoImage(sign_up_button_resized)
@@ -189,7 +202,7 @@ class ForgotPassword(tk.Frame):
         subtitle = tk.Label(self, text="Don't worry, it happens to the best of us. Enter the username you are using\n\nbelow and we will get your account back in a jiffy.", font=(TRUE_FONT, 8), background='#FFFFFF', foreground='#757575')
         
         # import entryline image
-        entryline_file = os.path.join(assetdir, 'entryline.png')
+        entryline_file = os.path.join(_assetdir, 'entryline.png')
         entryline_image = Image.open(entryline_file)
         entryline_resized = entryline_image.resize((250, 26), Image.ANTIALIAS)
         entryline_final = ImageTk.PhotoImage(entryline_resized)
@@ -201,7 +214,7 @@ class ForgotPassword(tk.Frame):
         username_text = tk.Label(self, text="Username", font=(TRUE_FONT, 8, "bold"), background='#FFFFFF', foreground='#757575')
         
         # request button
-        request_path = os.path.join(assetdir, 'request_password.png')
+        request_path = os.path.join(_assetdir, 'request_password.png')
         request_image = Image.open(request_path)
         request_resized = request_image.resize((250, 47), Image.ANTIALIAS)
         request_final = ImageTk.PhotoImage(request_resized)
@@ -237,12 +250,20 @@ class AnswerSecurityQuestions(tk.Frame):
         # set background color
         self.config(bg='#FFFFFF')
         
+        # import logo 
+        iconfile = os.path.join(_assetdir, 'black_noodles_black.png')
+        image = Image.open(iconfile)
+        logo_resized = image.resize((250, 250), Image.ANTIALIAS)     
+        img = ImageTk.PhotoImage(logo_resized)
+        logo = tk.Label(self, image=img, background = '#FFFFFF')
+        logo.image = img # prevent garbage collection
+        
         # title
         title = tk.Label(self, text="Reset Your Password", font=(TRUE_FONT, 18), background='#FFFFFF', foreground='#000000')
         subtitle = tk.Label(self, text="Please enter your new password below.", font=(TRUE_FONT, 8), background='#FFFFFF', foreground='#757575')
         
         # import entryline image
-        entryline_file = os.path.join(assetdir, 'entryline.png')
+        entryline_file = os.path.join(_assetdir, 'entryline.png')
         entryline_image = Image.open(entryline_file)
         entryline_resized = entryline_image.resize((250, 26), Image.ANTIALIAS)
         entryline_final = ImageTk.PhotoImage(entryline_resized)
@@ -251,25 +272,89 @@ class AnswerSecurityQuestions(tk.Frame):
         pw_entryline = tk.Label(self, image=entryline_final, background = '#FFFFFF')
         pw_entryline.image = entryline_final
         pw_entry = tk.Entry(self, borderwidth=0, show="◕", width=40, background='#FFFFFF', foreground='#757575', insertbackground='#757575') #show="*" changes input to *
-        pw_text = tk.Label(self, text="Enter New Password", font=(TRUE_FONT, 8), background='#FFFFFF', foreground='#757575')
+        pw_text = tk.Label(self, text="Enter New Password", font=(TRUE_FONT, 8, "bold"), background='#FFFFFF', foreground='#757575')
         
         # confirmation entry
         pw_confirm_entryline = tk.Label(self, image=entryline_final, background = '#FFFFFF')
         pw_confirm_entryline.image = entryline_final
         pw_confirm_entry = tk.Entry(self, borderwidth=0, show="◕", width=40, background='#FFFFFF', foreground='#757575', insertbackground='#757575') #show="*" changes input to *
-        pw_confirm_text = tk.Label(self, text="Confirm Password", font=(TRUE_FONT, 8), background='#FFFFFF', foreground='#757575')
+        pw_confirm_text = tk.Label(self, text="Confirm Password", font=(TRUE_FONT, 8, "bold"), background='#FFFFFF', foreground='#757575')
+        
+        # security question 1 dropdown
+        dropdown_1_text = tk.Label(self, text="Security Question 1", font=(TRUE_FONT, 8), background='#FFFFFF', foreground='#757575')
+        var_1 = tk.StringVar()
+        dropdown_1 = tk.OptionMenu(self, var_1, *_security_questions_1)
+        dropdown_1.config(width=34, background='#FFFFFF', activebackground='#FFFFFF', anchor=tk.W, borderwidth=1, relief="ridge")
+        dropdown_1["menu"].config(background='#FFFFFF', foreground='#757575', activebackground='#FFFFFF', activeforeground='#000000')
+        
+        # security question 1 response
+        response_1_entryline = tk.Label(self, image=entryline_final, background = '#FFFFFF')
+        response_1_entryline.image = entryline_final
+        response_1_entry = tk.Entry(self, borderwidth=0, width=40, background='#FFFFFF', foreground='#757575', insertbackground='#757575')
+        response_1_text = tk.Label(self, text="Your Answer", font=(TRUE_FONT, 8), background='#FFFFFF', foreground='#757575')
+        
+        # security question 2 dropdown
+        dropdown_2_text = tk.Label(self, text="Security Question 2", font=(TRUE_FONT, 8), background='#FFFFFF', foreground='#757575')
+        var_2 = tk.StringVar()
+        dropdown_2 = tk.OptionMenu(self, var_2, *_security_questions_2)
+        dropdown_2.config(width=34, background='#FFFFFF', activebackground='#FFFFFF', anchor=tk.W, borderwidth=1, relief="ridge")
+        dropdown_2["menu"].config(background='#FFFFFF', foreground='#757575', activebackground='#FFFFFF', activeforeground='#000000')
+        
+        # security question 2 response
+        response_2_entryline = tk.Label(self, image=entryline_final, background = '#FFFFFF')
+        response_2_entryline.image = entryline_final
+        response_2_entry = tk.Entry(self, borderwidth=0, width=40, background='#FFFFFF', foreground='#757575', insertbackground='#757575')
+        response_2_text = tk.Label(self, text="Your Answer", font=(TRUE_FONT, 8), background='#FFFFFF', foreground='#757575')
+        
+        # confirm button
+        confirm_button_path = os.path.join(_assetdir, 'confirm_small.png')
+        confirm_button_image = Image.open(confirm_button_path)
+        confirm_button_resized = confirm_button_image.resize((143, 47), Image.ANTIALIAS)
+        confirm_button_final = ImageTk.PhotoImage(confirm_button_resized)
+        confirm_button = tk.Button(self, image=confirm_button_final, padx=10, pady=10, command=self.on_confirm_press, background='#FFFFFF', borderwidth=0)
+        confirm_button.image = confirm_button_final # prevent garbage collection
+        
+        # cancel button
+        cancel_button_path = os.path.join(_assetdir, 'cancel_small.png')
+        cancel_button_image = Image.open(cancel_button_path)
+        cancel_button_resized = cancel_button_image.resize((143, 47), Image.ANTIALIAS)
+        cancel_button_final = ImageTk.PhotoImage(cancel_button_resized)
+        cancel_button = tk.Button(self, image=cancel_button_final, padx=10, pady=10, command=lambda: controller.show_frame(StartPage), background='#FFFFFF', borderwidth=0)
+        cancel_button.image = cancel_button_final # prevent garbage collection
         
         # placement
-        title.place(x=55, y=130)
-        subtitle.place(x=145, y=160)
+        title.place(x=75, y=30)
+        subtitle.place(x=75, y=70)
         
-        pw_text.place(x=75, y=250)
-        pw_entry.place(x=77, y=270)
-        pw_entryline.place(x=70, y=260)
+        logo.place(x=445, y=70)
         
-        pw_confirm_text.place(x=75, y=300)
-        pw_confirm_entry.place(x=77, y=320)
-        pw_confirm_entryline.place(x=70, y=310)
+        confirm_button.place(x=500, y=360)
+        cancel_button.place(x=500, y=410)
+        
+        pw_text.place(x=75, y=120)
+        pw_entry.place(x=77, y=140)
+        pw_entryline.place(x=70, y=130)
+        
+        pw_confirm_text.place(x=75, y=170)
+        pw_confirm_entry.place(x=77, y=190)
+        pw_confirm_entryline.place(x=70, y=180)
+        
+        dropdown_1_text.place(x=75, y=230)
+        dropdown_1.place(x=74, y=250)
+        
+        response_1_text.place(x=75, y=290)
+        response_1_entry.place(x=77, y=310)
+        response_1_entryline.place(x=70, y=300)
+        
+        dropdown_2_text.place(x=75, y=350)
+        dropdown_2.place(x=74, y=370)
+        
+        response_2_text.place(x=75, y=410)
+        response_2_entry.place(x=77, y=430)
+        response_2_entryline.place(x=70, y=420)
+        
+    def on_confirm_press(self):
+        _reset_password(self.response_1_entry.get(), self.response_2_entry.get(), self.pw_entry.get())
 
 
 class SignUp(tk.Frame):
@@ -280,7 +365,7 @@ class SignUp(tk.Frame):
         self.config(bg='#FFFFFF')
         
         # import logo 
-        iconfile = os.path.join(assetdir, 'black_noodles_black.png')
+        iconfile = os.path.join(_assetdir, 'black_noodles_black.png')
         image = Image.open(iconfile)
         logo_resized = image.resize((100, 100), Image.ANTIALIAS)     
         img = ImageTk.PhotoImage(logo_resized)
@@ -288,7 +373,7 @@ class SignUp(tk.Frame):
         logo.image = img # prevent garbage collection
         
         # banner
-        banner_file = os.path.join(assetdir, 'sign_up_banner.png')
+        banner_file = os.path.join(_assetdir, 'sign_up_banner.png')
         banner_image = Image.open(banner_file)
         banner_resized = banner_image.resize((543, 540), Image.ANTIALIAS)
         banner_final = ImageTk.PhotoImage(banner_resized)
@@ -299,7 +384,7 @@ class SignUp(tk.Frame):
         banner.create_text(205, 210, fill='#FFFFFF', font=(TRUE_FONT, 8), text="Insert here some inspirational text about \nwhy its a good idea to protect your passwords.")
         
         # sign up button
-        sign_up_button_path = os.path.join(assetdir, 'get_started.png')
+        sign_up_button_path = os.path.join(_assetdir, 'get_started.png')
         sign_up_button_image = Image.open(sign_up_button_path)
         sign_up_button_resized = sign_up_button_image.resize((250, 47), Image.ANTIALIAS)
         sign_up_button_final = ImageTk.PhotoImage(sign_up_button_resized)
@@ -311,7 +396,7 @@ class SignUp(tk.Frame):
         subtitle = tk.Label(self, text="Create an account", font=(TRUE_FONT, 8), foreground='#757575', background='#FFFFFF')
         
         # import entryline image
-        entryline_file = os.path.join(assetdir, 'entryline.png')
+        entryline_file = os.path.join(_assetdir, 'entryline.png')
         entryline_image = Image.open(entryline_file)
         entryline_resized = entryline_image.resize((250, 26), Image.ANTIALIAS)
         entryline_final = ImageTk.PhotoImage(entryline_resized)
@@ -369,11 +454,11 @@ if __name__ == "__main__":
     
     # set icon
     if platform.system() == 'Windows':
-        iconfile = os.path.join(assetdir, 'black_noodles_white_Xbg_icon.ico')
+        iconfile = os.path.join(_assetdir, 'black_noodles_white_Xbg_icon.ico')
         application_process.wm_iconbitmap(default=iconfile)
     else:
         ext = '.png' if tk.TkVersion >= 8.6 else '.gif'
-        iconfiles = [os.path.join(assetdir, 'black_noodles_white_Xbg_icon%s' % (ext))]
+        iconfiles = [os.path.join(_assetdir, 'black_noodles_white_Xbg_icon%s' % (ext))]
         icons = [tk.PhotoImage(master=application_process, file=iconfile) for iconfile in iconfiles]
         application_process.wm_iconphoto(True, *icons)
 
