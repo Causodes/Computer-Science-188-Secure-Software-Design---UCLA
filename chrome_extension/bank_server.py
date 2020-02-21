0ca4dc9e38af358733c624ae3aa010da75a712e8
kli512 <li.kevin512@gmail.com>
2020-02-21 00:52:39 -0800

    oh wow one last try

--- chrome_extension/bank_server.py
+++ chrome_extension/bank_server.py
@@ -104,6 +104,8 @@ class BankServer():
 
             await l_queue.async_q.put(text)
 
+            print(f'Put it in queue!', file=sys.stderr, flush=True)
+
     async def _write_client(self, writer: asyncio.StreamWriter, cli_addr: str) -> None:
         """Writes messages on writer from respective bank_messages queue
 
